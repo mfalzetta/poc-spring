b3b9a169e11b64b5b84f21f1a80084ec47df7c75
mfalzetta <marlon.falzetta@gmail.com>
2016-03-02 11:06:40 -0300

    Teste com Spring Rest

---
+++ spring-rest/src/main/java/br/com/matta/whois/Whois.java
@@ -0,0 +1,14 @@
+package br.com.matta.whois;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Whois {
+
+	@RequestMapping("/whoisfake")
+    public String getAsNumber(@RequestParam(value="ip", defaultValue="201.69.33.209") String name) {
+        return "AS | IP               | AS Name\n27699   | 201.69.33.209    | Telefônica Brasil S.A,BR";
+    }
+}
